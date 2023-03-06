f9230e9d5f9b6d0a835727cc09737bd1da52ce16
marksikaundi <emmanuelmark117@gmail.com>
2023-03-07 00:14:30 +0200

    fixing bugs

--- index.js
+++ index.js
@@ -24,13 +24,13 @@ app.post('/register', async (req,res) => {
     const {username,password} = req.body;
     try {
         const createdUser = await User.create({username,password});
+        jwt.sign({userId:createdUser,_id}, jwtSecret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).status(201).json('ok');
+        });
     } catch(err) {
         if (err) throw err;
     }
-    jwt.sign({userId:createdUser,_id}, jwtSecret, {}, (err, token) => {
-        if (err) throw err;
-        res.cookie('token', token).status(201).json('ok');
-    });
 });
 
 app.listen(4000);
