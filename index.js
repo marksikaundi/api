3a71aebb1afa22266432f79bd60ca2fc5a556a6a
marksikaundi <emmanuelmark117@gmail.com>
2023-03-06 23:21:26 +0200

    configuration done

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ app.get('/test', (req,res) => {
 app.post('/register', async (req,res) => {
     const {username,password} = req.body;
     const createdUser = await User.create({username,password});
-    jwt.sign({userId:createdUser,_id},jwtSecret).then()
+    jwt.sign({userId:createdUser,_id}, jwtSecret, (err, token) => {
+        if (err) throw err;
+        res.cookie('token', token).status(201).json('ok');
+    });
 });
 
 app.listen(4000);
