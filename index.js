4d12c53026ecf6de545c6dd5233cd1f6523f6a45
marksikaundi <emmanuelmark117@gmail.com>
2023-03-07 00:02:39 +0200

    debugging api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
+const cors = require('cors'); 
 const User = require('./models/User');
 const { response } = require('express');
 
@@ -10,6 +11,10 @@ mongoose.connect(process.env.MONGO_URL);
 const jwtSecret = process.env.JWT_SECRET;
 
 const app = express();
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+}));
 
 app.get('/test', (req,res) => {
     res.json('test ok');
@@ -17,8 +22,12 @@ app.get('/test', (req,res) => {
 
 app.post('/register', async (req,res) => {
     const {username,password} = req.body;
-    const createdUser = await User.create({username,password});
-    jwt.sign({userId:createdUser,_id}, jwtSecret, (err, token) => {
+    try {
+        const createdUser = await User.create({username,password});
+    } catch(err) {
+        if (err) throw err;
+    }
+    jwt.sign({userId:createdUser,_id}, jwtSecret, {}, (err, token) => {
         if (err) throw err;
         res.cookie('token', token).status(201).json('ok');
     });
--- models/User.js
+++ models/User.js
@@ -6,4 +6,4 @@ const UserSchema = new mongoose.Schema({
 }, {timestamps: true});
 
 const UserModel = mongoose.model('User', UserSchema);
-module.exports =UserModel;
+module.exports = UserModel;
